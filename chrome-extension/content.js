d7fe1a2ad10e84f27c157e4ecc9d192cb35330fb
Mohammed Sohail <sohailsameja@gmail.com>
2019-10-12 04:21:14 +0530

    extension: added support for tmdb

--- chrome-extension/content.js
+++ chrome-extension/content.js
@@ -1,18 +1,25 @@
 // content.js
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   switch (request.type) {
-    case "openMovieModal": {
-      // Add code to show modal
-      console.log('Adding modal!');
-      $.get(chrome.extension.getURL('modal.html'), data => {
-        $($.parseHTML(data)).appendTo('body').slideDown();;
-        $(document).on('click', () => {
-          const dataObj = $('#moodx-movie-suggest-card');
-          console.log('clicked fuk');
-          if (dataObj.length)
-            dataObj.slideUp(() => dataObj.remove());
+    case 'openMovieModal': {
+      $.get('https://api.themoviedb.org/3/movie/?api_key=', res => {
+        $.get(chrome.extension.getURL('modal.html'), data => {
+          $($.parseHTML(data)).appendTo('body').slideDown();
+
+          $('#title').text(`${res.original_title} (${res.release_date.split('-')[0]})`);
+          $('#runtime').text(`${res.runtime} min`);
+          $('#genres').text(res['genres'].map(e => e.name).join(', '));
+          $('#overview').text(res.overview);
+          $('#poster').attr('src', `https://image.tmdb.org/t/p/original${res.poster_path}`);
+          $(".bright_back").css("background-image", `url(https://image.tmdb.org/t/p/w400${res.backdrop_path}`);
+
+          $(document).on('click', () => {
+            const dataObj = $('#moodx-movie-suggest-card');
+            if (dataObj.length)
+              dataObj.slideUp(() => dataObj.remove());
+          });
         });
-      });
+      })
       break;
     }
   }
